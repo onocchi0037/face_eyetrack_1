b520ee34d8871a9be867804abb5f3adacbe8dbc0
onocchi0073 <onocchi0073@yahoo.co.jp>
2021-05-02 20:08:37 +0900

    second

--- dev_script_0.1/accuracy_test/eye-track_yokubaranai_1920_1080.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-
-#. ~/eye-track/bin/activate
-python accuracy_test_demo.3.py --scale_factor 0.4 --model 101 --disp_resolution_h 1080 --disp_resolution_w 1920 --view 1 --disp_size_w 55 --disp_size_h 32  --cam_id 0 
-
